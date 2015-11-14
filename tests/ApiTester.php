72b0bb10ebd259c5ecb760d95930a9f0ae6a37b2
Flavio Ferreira <flaviometalvale@gmail.com>
2015-11-14 02:20:15 -0200

    Fix test class

--- tests/ApiTester.php
+++ tests/ApiTester.php
@@ -16,9 +16,25 @@ class ApiTester extends TestCase
         $this->fake = Faker::create();
     }
 
+    public function setUp()
+    {
+        $app = $this->createApplication();
+        //$app->make('artisan')->call('migrate');
+        Artisan::call('migrate');
+
+        parent::setUp();
+    }
+
     public function times($count)
     {
         $this->times = $count;
+
+        return $this;
+    }
+
+    public function getJson($uri)
+    {
+        return json_encode($this->call('GET', $uri)->getContent());
     }
 
 }
